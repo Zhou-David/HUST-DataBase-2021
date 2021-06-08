1e2bade9ef42d0bc167a72dfdc2939f15db4bbab
大伟 周 <1696950216@qq.com>
2021-06-08 09:40:09 +0800

    晚上医师管理界面

--- 医院管理系统/src/HospitalManagementSystem/controller/DoctorServiceController.java
+++ 医院管理系统/src/HospitalManagementSystem/controller/DoctorServiceController.java
@@ -1,9 +1,7 @@
 package HospitalManagementSystem.controller;
 
 import HospitalManagementSystem.function.Func;
-import HospitalManagementSystem.model.Department;
-import HospitalManagementSystem.model.Doctor;
-import HospitalManagementSystem.model.Main;
+import HospitalManagementSystem.model.*;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -26,20 +24,74 @@ import java.util.Date;
 import java.util.ResourceBundle;
 
 public class DoctorServiceController implements Initializable {
-    public Button btnReturn;
+    /**
+     * 返回按钮1
+     */
     public Button btnReturn1;
+
+    /**
+     * 是否为专家选择框
+     */
     public CheckBox checkExpert;
+
+    /**
+     * 个人信息-姓名
+     */
     public TextField fieldName;
+
+    /**
+     * 个人信息-性别
+     */
     public ChoiceBox<String> cbxSex;
+
+    /**
+     * 个人信息-出生日期
+     */
     public DatePicker dateBirthday;
+
+    /**
+     * 个人信息-工作日期
+     */
     public DatePicker dateWorkingDay;
+
+    /**
+     * 个人信息-所属科室
+     */
     public ChoiceBox<String> cbxDepartment;
+
+    /**
+     * 个人信息-职务
+     */
     public TextField fieldJob;
+
+    /**
+     * 个人信息-电话号码
+     */
     public TextField fieldPhone;
+
+    /**
+     * 个人信息-电子邮件
+     */
     public TextField fieldEmail;
+
+    /**
+     * 个人信息-挂号费
+     */
     public TextField fieldRegisterFee;
+
+    /**
+     * 个人信息提交按钮
+     */
     public Button btnSubmit;
+
+    /**
+     * 医师信息表
+     */
     public TableView<Doctor> tabDoctorInfo;
+
+    /**
+     * 医师信息表中的各个列
+     */
     public TableColumn<Doctor,String> colDoctorName;
     public TableColumn<Doctor,String> colDoctorSex;
     public TableColumn<Doctor,String> colDoctorBirthday;
@@ -50,10 +102,116 @@ public class DoctorServiceController implements Initializable {
     public TableColumn<Doctor,String> colDoctorPone;
     public TableColumn<Doctor,String> colDoctorEmail;
     public TableColumn<Doctor,String> colDoctorExpert;
+
+    /**
+     * 搜索医师的姓名
+     */
     public TextField fieldDoctorSearch;
+
+    /**
+     * 搜索医师按钮
+     */
     public Button btnDoctorSearch;
+
+    /**
+     * 返回按钮6
+     */
     public Button btnReturn6;
+
+    /**
+     * 药品信息表
+     */
+    public TableView<Medicine> tabMedicineInfo;
+
+    /**
+     * 药品信息的各个列
+     */
+    public TableColumn<Medicine,String> colMedicineName;
+    public TableColumn<Medicine,String> colMedicineDosage;
+    public TableColumn<Medicine,String> colMedicineSpecifications;
+    public TableColumn<Medicine,String> colMedicineIntroduction;
+    public TableColumn<Medicine,Integer> colMedicinePrice;
+    public TableColumn<Medicine,String> colMedicineType;
+
+    /**
+     * 搜索药品的名称
+     */
+    public TextField fieldMedicineSearch;
+
+    /**
+     * 搜索药品按钮
+     */
+    public Button btnMedicineSearch;
+
+    /**
+     * 返回按钮3
+     */
+    public Button btnReturn3;
+
+    /**
+     * 病房信息表
+     */
+    public TableView<Ward> tabWardInfo;
+
+    /**
+     * 病房信息表的各个列
+     */
+    public TableColumn<Ward,String> colWardRoomNum;
+    public TableColumn<Ward,Integer> colWardCapacity;
+    public TableColumn<Ward,String> colWardType;
+    public TableColumn<Ward,Integer> colWardUsed;
+    public TableColumn<Ward,String> colWardRemarks;
+
+    /**
+     * 搜索病房信息的房间号
+     */
+    public TextField fieldWardSearch;
+
+    /**
+     * 搜索病房信息按钮
+     */
+    public Button btnWardSearch;
+
+    /**
+     * 返回按钮4
+     */
+    public Button btnReturn4;
+
+    /**
+     * 科室信息表
+     */
+    public TableView<Department> tabDepartmentInfo;
+
+    /**
+     * 科室信息表的各个列
+     */
+    public TableColumn<Department,Integer> colDepartmentId;
+    public TableColumn<Department,String> colDepartmentName;
+    public TableColumn<Department,String> colDepartmentDean;
+
+    /**
+     * 搜索科室信息的名称
+     */
+    public TextField fieldDepartmentSearch;
+
+    /**
+     * 搜索科室按钮
+     */
+    public Button btnDepartmentSearch;
+
+    /**
+     * 返回按钮7
+     */
+    public Button btnReturn7;
+
+    /**
+     * 医师服务界面APP
+     */
     private Main doctorServiceApp;
+
+    /**
+     * 此医师的识别编号
+     */
     private int doctorID;
 
     /**
@@ -66,6 +224,21 @@ public class DoctorServiceController implements Initializable {
      */
     private final ObservableList<Department> departmentData = FXCollections.observableArrayList();
 
+    /**
+     * 从数据库导入的病房信息数据
+     */
+    private final ObservableList<Ward> wardData = FXCollections.observableArrayList();
+
+    /**
+     * 从数据库导入的药品信息数据
+     */
+    private final ObservableList<Medicine> medicineData = FXCollections.observableArrayList();
+
+    /**
+     * 设置医师服务界面
+     * @param doctorServiceApp:医师服务界面APP
+     * @param doctorID:该医师的识别编号
+     */
     public void setDoctorServiceApp(Main doctorServiceApp,int doctorID){
         this.doctorServiceApp=doctorServiceApp;
         this.doctorID=doctorID;
@@ -85,6 +258,7 @@ public class DoctorServiceController implements Initializable {
                 cbxDepartment.getItems().add(department.getDepartmentName());
                 departmentData.add(department);
             }
+            setDepartmentData(departmentData);
         }catch (SQLException e) {
             e.printStackTrace();
         }
@@ -109,6 +283,7 @@ public class DoctorServiceController implements Initializable {
                     fieldPhone.setText(doctor.getPhoneNumber());
                     cbxDepartment.setValue(doctor.getDepartment());
                     cbxSex.setValue(doctor.getSex());
+                    checkExpert.setSelected(doctor.getIsExpert());
 
                     Date date = new Date(doctor.getBirthday().getTime());
                     LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
@@ -126,18 +301,68 @@ public class DoctorServiceController implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        //在药品管理中导入数据
+        try {
+            String sql;
+            sql = "SELECT * FROM 药品信息";
+            ResultSet rs = Func.statement.executeQuery(sql);
+            while (rs.next()) {
+                Medicine medicine=new Medicine(
+                        rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),
+                        rs.getString(5),rs.getInt(6),rs.getString(7)
+                );
+                medicineData.add(medicine);
+            }
+            setMedicineData(medicineData);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //在病房管理中导入数据
+        try {
+            String sql;
+            sql = "SELECT * FROM 病房信息";
+            ResultSet rs = Func.statement.executeQuery(sql);
+            while (rs.next()) {
+                Ward ward=new Ward(
+                        rs.getInt(1),rs.getString(2),rs.getInt(3),
+                        rs.getString(4),rs.getInt(5),rs.getString(6)
+                );
+                wardData.add(ward);
+            }
+            setWardData(wardData);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * 返回上一级
+     * @param event:触发时间
+     */
     public void onClickReturn(ActionEvent event) {
         this.doctorServiceApp.gotoMain();
     }
 
+    /**
+     * 信息修改提交
+     * @param event:触发事件
+     */
     public void onClickSubmit(ActionEvent event) {
     }
 
+    /**
+     * 医师搜索
+     * @param event:触发事件
+     */
     public void onClickDoctorSearch(ActionEvent event) {
     }
 
+    /**
+     * 医师数据导入
+     * @param doctorData:医师数据
+     */
     public void setDoctorData(ObservableList<Doctor> doctorData) {
         //确定数据导入的列
         colDoctorName.setCellValueFactory(
@@ -173,6 +398,81 @@ public class DoctorServiceController implements Initializable {
 
         //向表中导入数据
         tabDoctorInfo.setItems(doctorData);
+    }
+
+    /**
+     * 科室数据导入
+     * @param departmentData:科室数据
+     */
+    public void setDepartmentData(ObservableList<Department> departmentData){
+        //确定导入数据的列
+        colDepartmentId.setCellValueFactory(
+                departmentIntegerCellDataFeatures -> new SimpleIntegerProperty(departmentIntegerCellDataFeatures.getValue().getId()).asObject()
+        );
+        colDepartmentDean.setCellValueFactory(
+                departmentStringCellDataFeatures -> new SimpleStringProperty(departmentStringCellDataFeatures.getValue().getDeanName())
+        );
+        colDepartmentName.setCellValueFactory(
+                departmentStringCellDataFeatures -> new SimpleStringProperty(departmentStringCellDataFeatures.getValue().getDepartmentName())
+        );
+
+        //向表中导入数据
+        tabDepartmentInfo.setItems(departmentData);
+    }
+
+    /**
+     * 药品数据导入
+     * @param medicineData:药品数据
+     */
+    private void setMedicineData(ObservableList<Medicine> medicineData) {
+        //确定数据导入的列
+        colMedicineName.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleStringProperty(medicineStringCellDataFeatures.getValue().getName())
+        );
+        colMedicineDosage.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleStringProperty(medicineStringCellDataFeatures.getValue().getDosageForm())
+        );
+        colMedicineSpecifications.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleStringProperty(medicineStringCellDataFeatures.getValue().getSpecifications())
+        );
+        colMedicineIntroduction.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleStringProperty(medicineStringCellDataFeatures.getValue().getIntroduction())
+        );
+        colMedicinePrice.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleIntegerProperty(medicineStringCellDataFeatures.getValue().getPrice()).asObject()
+        );
+        colMedicineType.setCellValueFactory(
+                medicineStringCellDataFeatures -> new SimpleStringProperty(medicineStringCellDataFeatures.getValue().getType())
+        );
+
+        //向表中导入数据
+        tabMedicineInfo.setItems(medicineData);
+    }
+
+    /**
+     * 病房数据导入
+     * @param wardData:病房数据
+     */
+    private void setWardData(ObservableList<Ward> wardData) {
+        //确定数据导入的列
+        colWardRoomNum.setCellValueFactory(
+                wardStringCellDataFeatures -> new SimpleStringProperty(wardStringCellDataFeatures.getValue().getNumber())
+        );
+        colWardCapacity.setCellValueFactory(
+                wardIntegerCellDataFeatures -> new SimpleIntegerProperty(wardIntegerCellDataFeatures.getValue().getCapacity()).asObject()
+        );
+        colWardType.setCellValueFactory(
+                wardStringCellDataFeatures -> new SimpleStringProperty(wardStringCellDataFeatures.getValue().getType())
+        );
+        colWardUsed.setCellValueFactory(
+                wardIntegerCellDataFeatures -> new SimpleIntegerProperty(wardIntegerCellDataFeatures.getValue().getUsed()).asObject()
+        );
+        colWardRemarks.setCellValueFactory(
+                wardStringCellDataFeatures -> new SimpleStringProperty(wardStringCellDataFeatures.getValue().getRemarks())
+        );
+
+        //向表中导入数据
+        tabWardInfo.setItems(wardData);
     }
 
     @Override
